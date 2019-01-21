c3157030934523606cb8663b5468ad02770368fa
sonhyungjung <thsgudwjd@gmail.com>
2019-01-21 19:22:20 +0900

    add 배열 array.js

---
+++ syntax/array.js
@@ -0,0 +1,10 @@
+//CRUD 배열
+var arr = ['A','B','C','D'];
+console.log(arr[1]);//0부터 카운트 a
+console.log(arr[3]); //b
+
+arr[2] = 3; 
+console.log(arr); 
+console.log(arr.length); //배열 개수
+arr.push('E'); //끝에 data ';E'출력 , 끝에 추가
+console.log(arr);
