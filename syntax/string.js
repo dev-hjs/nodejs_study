23017f918c2d1312b4a290f9b075dd590135e034
sonhyungjung <thsgudwjd@gmail.com>
2019-01-21 18:04:18 +0900

    add commit

---
+++ syntax/string.js
@@ -0,0 +1,3 @@
+//문자열
+console.log('1'+'1');//문자열과 문자열을 더한다
+console.log('lorem Enim dolore mollit voluptate culpa ad esse exercitation elit duis reprehenderit nisi.'.length);
---
+++ syntax/template.js
@@ -0,0 +1,5 @@
+var name = 'k8805';
+var letter = `dear ${name}
+
+Id duis ipsum laborum '+name+' cupidatat esse cillum velit enim ullamco est est occaecat labore ullamco. ${name}`;
+console.log(letter);
