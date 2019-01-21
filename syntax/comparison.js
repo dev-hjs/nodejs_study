3b140b445319ea172eea6121fdcedf81e4bf83dc
sonhyungjung <thsgudwjd@gmail.com>
2019-01-21 18:31:42 +0900

    add 비교연산자 comparison.js

---
+++ syntax/comparison.js
@@ -0,0 +1,9 @@
+//비교연산자
+console.log(1==1);//1과 1이 같은가?   1과 1이 같으면 true
+console.log(1==2);//false
+console.log(1>2);//false
+console.log(1<2); //false
+console.log(1===1); //정확하게 같은가? 삼항연산자 //true
+console.log(1===2); //false
+
+name === 1; //비교 연산자 
